000902dc00f8cb616d7aa59525496de58d568daa
sfstar <sventenraa@hotmail.com>
2025-01-29 19:18:35 +0000

    resolve the linting errors for the base.py file

--- custom_components/victron/base.py
+++ custom_components/victron/base.py
@@ -1,22 +1,4 @@
-"""Module defines entity descriptions for Victron components.
-
-Classes:
-    VictronBaseEntityDescription: Describes a base entity for Victron components.
-    VictronWriteBaseEntityDescription: Describes a writable base entity for Victron components.
-
-VictronBaseEntityDescription:
-    Attributes:
-        slave (int): The slave identifier.
-        value_fn (Callable[[dict], StateType]): A function to extract the value from data.
-
-Methods:
-        lambda_func(): Returns a lambda function to extract data based on slave and key.
-
-VictronWriteBaseEntityDescription:
-    Attributes:
-        address (int): The address for the writable entity.
-
-"""
+"""Module defines entity descriptions for Victron components."""
 
 from collections.abc import Callable
 from dataclasses import dataclass
@@ -27,8 +9,11 @@ from homeassistant.helpers.typing import StateType
 
 @dataclass
 class VictronBaseEntityDescription(EntityDescription):
+    """An extension of EntityDescription for Victron components."""
+
     @staticmethod
     def lambda_func():
+        """Return an entitydescription."""
         return lambda data, slave, key: data["data"][str(slave) + "." + str(key)]
 
     slave: int = None
@@ -37,4 +22,6 @@ class VictronBaseEntityDescription(EntityDescription):
 
 @dataclass
 class VictronWriteBaseEntityDescription(VictronBaseEntityDescription):
+    """An extension of VictronBaseEntityDescription for writeable Victron components."""
+
     address: int = None
